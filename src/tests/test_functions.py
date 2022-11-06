9c5c616e0dc63e618b81456bacd2f7212dea18ea
Andrew Muth <agmuth@gmail.com>
2022-11-06 13:38:23 -0700

    added test functions and fixed format to make easier going forward

--- src/tests/test_functions.py
+++ src/tests/test_functions.py
@@ -2,12 +2,30 @@ import numpy as np
 
 
 class TestFunction(object):
-    def __init__(self, func: callable, dim: int, minimum: np.array):
-        self.func, self.dim, self.minimum = func, dim, minimum
+    def __init__(self, func: callable, x_0: np.array, x_minimum: np.array, func_minimum: float):
+        self.func, self.x_0, self.x_minimum, self.func_minimum = func, x_0, x_minimum, func_minimum
 
 
-axis_parallel_hyper_ellipsoid_function_2_d = TestFunction(
-    func=lambda x: np.dot(np.arange(1, x.shape[0]+1), x**2),
-    dim=2,
-    minimum=np.array([0., 0.])
+sphere_function_2d = TestFunction(
+    func=lambda x: (x**2).sum(),
+    x_0=np.array([-5.12, 5.12]),
+    x_minimum=np.array([0., 0.]),
+    func_minimum=0.
 )
+
+
+axis_parallel_hyper_ellipsoid_function_3d = TestFunction(
+    func=lambda x: np.dot(np.arange(1, x.shape[0]+1), x**2),
+    x_0=np.array([5.12, 5.12, 5.12]),
+    x_minimum=np.array([0., 0., 0.]),
+    func_minimum=0.
+)
+
+
+rotated_hyper_ellipsoid_function_4d = TestFunction(
+    func=lambda x: np.cumsum(x**2).sum(),
+    x_0=np.array([-65.536, 65.536, -65.536, 65.536]),
+    x_minimum=np.array([0., 0., 0., 0]),
+    func_minimum=0.
+)
+
--- src/tests/unconstrained_test.py
+++ src/tests/unconstrained_test.py
@@ -3,14 +3,26 @@ from convexoptimization.unconstrained import *
 import numpy as np
 
 
-def test_newtons_method_axis_parallel_hyper_ellipsoid_function_2_d():
-    tol = 1e-4
-    x_0 = np.array([5.12, 5.12])
-    res = newtons_method(
-        axis_parallel_hyper_ellipsoid_function_2_d.func, 
-        x_0
+def wrapper(solver: callable, test_func: TestFunction, tol: float=1e-4) -> None:
+    res = solver(
+        test_func.func, 
+        test_func.x_0
     )
-    assert res["convergence"] and np.linalg.norm(res["x"] - axis_parallel_hyper_ellipsoid_function_2_d.minimum, 2) < tol
+    assert all(
+        [
+            res["convergence"],
+             np.linalg.norm(res["x"] - test_func.x_minimum, 2) < tol,
+             abs(res["func"] - test_func.func_minimum) < tol
+
+        ]
+    )
 
 
+def test_newtons_method_sphere_function_2d():
+    wrapper(newtons_method, sphere_function_2d)
 
+def test_newtons_method_axis_parallel_hyper_ellipsoid_function_3d():
+    wrapper(newtons_method, axis_parallel_hyper_ellipsoid_function_3d)
+
+def test_newtons_method_rotated_hyper_ellipsoid_function_4d():
+    wrapper(newtons_method, rotated_hyper_ellipsoid_function_4d)
